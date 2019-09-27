5833aeac3f223af9bad756e9bb2e6f354a43f4f8
Solidajenjo <solidajenjo@gmail.com>
2019-09-27 18:12:21 +0200

    6502 Instruction set OPCodes, parse size & cycles set

--- DragoNES/DragoNES/src/CPU6502.h
+++ DragoNES/DragoNES/src/CPU6502.h
@@ -2,14 +2,28 @@
 #define __CPU6502_H_
 
 #include "Module.h"
+#include <string>
 
 class CPU6502 : public Module
 {
 public:
+
+	struct Instruction {
+		Instruction() {}
+		Instruction(char op, char length, char cycles, std::string text) : op(op), length(length), cycles(cycles), text(text) {}
+		char op, length, cycles;
+		std::string text;
+	};
+
 	CPU6502();
 	~CPU6502();
 
 	bool Start() override;
+
+	void CreateInstructionArray();
+
+private:
+	Instruction instructions[256];
 };
 #endif
 
